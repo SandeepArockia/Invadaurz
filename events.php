865881bac75f71dcad811fe4ba630d89d144935a
Pooventhiran <pooventhiran_g@icloud.com>
2017-10-10 21:48:15 +0530

    many

--- events.php
+++ events.php
@@ -5,25 +5,30 @@
   <link rel="icon" href="img/ITALogo.jpeg">
   <meta charset="utf-8">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link type="text/css" rel="stylesheet" href="css\materialize.min.css"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-  <link rel="stylesheet" href="css\animate.css">
+  <link rel="stylesheet" href="css/animate.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="js\materialize.min.js"></script>
+  <script src="js/materialize.min.js"></script>
   <script>
   $(document).ready(function(){
     $(".button-collapse").sideNav()
   });
   </script>
   <style>
+  img#eventLogo{
+    transition: color 0.8s ;
+    -webkit-transition: color 0.8s;
+  }
+  img#eventLogo:hover{
+    box-shadow: 1px 1px 5px 5px;
+    color: #448aff;
+  }
   .icon-border{
     border-style: solid;
     border-radius: 25px;
     border-width: 3px;
-  }
-  img.icon-border:hover{
-
   }
   body {
     display: flex;
@@ -80,28 +85,28 @@
           <div class="row">
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/BugBoss.php" > <img src="img\tech\BugBoss.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/BugBoss.php"> <img src="img/tech/BugBoss.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Bug Boss"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/CaptureTheFlag.php" > <img src="img\tech\CaptureTheFlag.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/CaptureTheFlag.php" > <img src="img/tech/CaptureTheFlag.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Capture The Flag"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp " >
-            <a href="event/tech/CodeRelay.php" > <img src="img\tech\CodeRelay.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/CodeRelay.php" > <img src="img/tech/CodeRelay.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Code Relay"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/ComputerGeeks.php" > <img src="img\tech\ComputerGeeks.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/ComputerGeeks.php" > <img src="img/tech/ComputerGeeks.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Computer Geeks"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/DOCTYPE.php" > <img src="img\tech\DOCTYPE.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/DOCTYPE.php" > <img src="img/tech/DOCTYPE.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="<!DOCTYPE>"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/ZestTech.php" > <img src="img\tech\ZestTech.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/ZestTech.php" > <img src="img/tech/ZestTech.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Zest Tech"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/Acute.php" > <img src="img\tech\Acute.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/Acute.php" > <img src="img/tech/Acute.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Acute"/> </a>
           </div>
           <div class="col l3 m6 s6 animated slideInUp">
-            <a href="event/tech/BubbleBugs.php" > <img src="img\tech\BubbleBug.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+            <a href="event/tech/BubbleBugs.php" > <img src="img/tech/BubbleBug.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Bubble Bug"/> </a>
           </div>
         </a>
       </div>
@@ -121,75 +126,34 @@
           <p> &nbsp; </p>
           <div class="row">
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/WheelOfFortune.php" > <img src="img/fun/WheelOfFortune.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/WheelOfFortune.php" > <img src="img/fun/WheelOfFortune.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Wheel of Fortune"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/RazzWhale.php" > <img src="img/fun/RazzWhale.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/RazzWhale.php" > <img src="img/fun/RazzWhale.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Razz Whale"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/Cryptica.php" > <img src="img/fun/Cryptica.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/Cryptica.php" > <img src="img/fun/Cryptica.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Cryptica"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/8-In-A-Maze.php" > <img src="img/fun/8.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/8-In-A-Maze.php" > <img src="img/fun/8.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="8-in-a-Maze"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/GameArena.php" > <img src="img/fun/GameArena.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/GameArena.php" > <img src="img/fun/GameArena.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Game Arena"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/FunBuzz.php" > <img src="img/fun/FunBuzz.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/FunBuzz.php" > <img src="img/fun/FunBuzz.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Fun Buzz"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/CrimeScene.php" > <img src="img/fun/CrimeScene.png" class="animated slideInLeft icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/CrimeScene.php" > <img src="img/fun/CrimeScene.png" class="animated slideInLeft icon-border white" id="eventLogo" style="width:100%" title="Crime Scene"/> </a>
             </div>
             <div class="col l3 m6 s6 animated fadeIn">
-              <a href="event/fun/Run-Some-Where.php" > <img src="img/fun/Run-Some-Where.png" class="animated slideInRight icon-border white" style="width:100%"/> </a>
+              <a href="event/fun/Run-Some-Where.php" > <img src="img/fun/Run-Some-Where.png" class="animated slideInRight icon-border white" id="eventLogo" style="width:100%" title="Run Somewhere"/> </a>
             </div>
           </div>
         </a>
       </div>
     </div>
   </main>
-
-  <div class="container">
-    <div class="row">
-      <div id="registerModal" class="modal">
-        <form class="col l12 m12 s12" action="">
-          <div class="modal-content">
-            <div class="row center">
-              Register
-            </div>
-            <div class="row">
-              <div class="input-field col l10 offset-l1 m10 offset-m1 s10 offset-s1">
-                <i class="prefix material-icons">account_circle</i>
-                <input id="user_name" type="text" class="validate" />
-                <label for="user_name">Username</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col l10 offset-l1 m10 offset-m1 s10 offset-s1">
-                <i class="prefix material-icons">mail_outline</i>
-                <input id="user_name" type="text" class="validate" />
-                <label for="user_name">Mail id</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col l10 offset-l1 m10 offset-m1 s10 offset-s1">
-                <i class="prefix material-icons">lock</i>
-                <input id="password" type="password" class="validate" />
-                <label for="password">Password</label>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="center">
-              <button type="submit" class="btn waves-effect waves-light green center">Register</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <footer class="page-footer ">
     <div class="container">
       INVADAURZ 2k17
